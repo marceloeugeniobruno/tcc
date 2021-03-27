f11f4a5a0d22af272ba33078632ca68c806cb007
marceloeugeniobruno <marceloeugeniobruno@gmail.com>
2021-03-27 12:56:48 -0300

    iniciando o git

--- app/src/main/java/com/example/tccflashcards/activity/FirstFragment.java
+++ app/src/main/java/com/example/tccflashcards/activity/FirstFragment.java
@@ -1,4 +1,4 @@
-package com.example.tccflashcards;
+package com.example.tccflashcards.activity;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.tccflashcards.R;
 
 public class FirstFragment extends Fragment {
 
